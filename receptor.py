03d7bf258c2e5d573550d9405df519852dcdf45d
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-06-01 21:39:02 -0500

    commit

--- receptor.py
+++ receptor.py
@@ -1,18 +1,16 @@
+# Receptor module: 
+# Irene Abad Londoño & Alejandro Hoyos Correa
+
+
 import paho.mqtt.client as mqtt
 import json
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("sda/sensor")
+    client.subscribe("sda/sensorIA")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg): 
-    #print(msg.topic+" "+str(msg.payload))
-    
     messageencoded = msg.payload
     messagedecoded = messageencoded.decode()
     message_dict = json.loads(messagedecoded)
@@ -31,10 +29,4 @@ client.on_message = on_message
 client.connect("broker.hivemq.com", 1883, 60)
 
 
-
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
